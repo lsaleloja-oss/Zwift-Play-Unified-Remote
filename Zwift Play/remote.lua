2dbc9657176351a0da72f0ce4875bd82f8396c63
Leving PT <lsaleloja@gmail.com>
2026-09-04 11:43:56 +0100

    Add remote control functionality for Zwift Play
    Implement keyboard actions for remote control functions.

---
+++ Zwift Play/remote.lua
@@ -0,0 +1,57 @@
+local kb = libs.keyboard;
+
+local downHeld = false;
+local camera = 1;
+
+--@help Left
+actions.left = function ()
+    kb.stroke("left");
+end
+
+--@help Up
+actions.up = function ()
+    kb.stroke("up");
+end
+
+--@help Right
+actions.right = function ()
+    kb.stroke("right");
+end
+
+--@help Down / U-turn
+actions.down = function ()
+    if downHeld then
+        kb.up("down");
+        downHeld = false;
+    else
+        kb.down("down");
+        downHeld = true;
+    end
+end
+
+--@help Confirm / Select
+actions.confirm = function ()
+    kb.stroke("enter");
+end
+
+--@help Previous Camera
+actions.camera_prev = function ()
+    camera = camera - 1;
+
+    if camera < 1 then
+        camera = 9;
+    end
+
+    kb.stroke(tostring(camera));
+end
+
+--@help Next Camera
+actions.camera_next = function ()
+    camera = camera + 1;
+
+    if camera > 9 then
+        camera = 1;
+    end
+
+    kb.stroke(tostring(camera));
+end
