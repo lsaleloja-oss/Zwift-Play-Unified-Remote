4b86b95ff7074af17b298bf9c1b84d95d609cf41
Leving PT <lsaleloja@gmail.com>
2026-09-22 08:08:10 +0100

    Add files via upload

--- Zwift Play/remote.lua
+++ Zwift Play/remote.lua
@@ -4,88 +4,77 @@ local downHeld = false;
 local camera = 1;
 
 --@help Left
-actions.left = function ()
-    kb.stroke("left");
-end
-
+actions.left = function () kb.stroke("left"); end
 --@help Up
-actions.up = function ()
-    kb.stroke("up");
-end
-
+actions.up = function () kb.stroke("up"); end
 --@help Right
-actions.right = function ()
-    kb.stroke("right");
-end
+actions.right = function () kb.stroke("right"); end
 
 --@help Down / U-turn
 actions.down = function ()
     if downHeld then
-        kb.up("down");
+        kb.stroke("down");
         downHeld = false;
     else
-        kb.down("down");
+        kb.stroke("down");
         downHeld = true;
+        -- solta sozinho após 2s para fazer U-turn no Zwift
+        libs.timer.timeout(function()
+            if downHeld then
+               kb.stroke("down");
+               downHeld = false;
+            end
+        end, 2000);
     end
 end
 
 --@help Confirm / Select
-actions.confirm = function ()
-    kb.stroke("enter");
-end
+actions.confirm = function () kb.stroke("enter"); end
 
 --@help Virtual Shifting
 actions.shift_down = function() kb.stroke("pagedown") end
 actions.shift_up = function() kb.stroke("pageup") end
 
---@help Previous Camera
+--@help Previous / Next Camera
 actions.camera_prev = function ()
     camera = camera - 1;
-
-    if camera < 1 then
-        camera = 9;
-    end
-
+    if camera < 1 then camera = 9; end
     kb.stroke(tostring(camera));
 end
-
---@help Next Camera
 actions.camera_next = function ()
     camera = camera + 1;
-
-    if camera > 9 then
-        camera = 1;
-    end
-
+    if camera > 9 then camera = 1; end
     kb.stroke(tostring(camera));
 end
 
 --@help Menu ESC
-actions.esc = function()
-    kb.stroke("esc");
-end
-
+actions.esc = function() kb.stroke("esc"); end
 --@help Screenshot F10
-actions.f10 = function()
-    kb.stroke("f10");
-end
-
+actions.f10 = function() kb.stroke("f10"); end
 --@help Pairing A
-actions.a_key = function()
-    kb.stroke("a");
-end
-
+actions.a_key = function() kb.stroke("a"); end
 --@help Workout E
-actions.e_key = function()
-    kb.stroke("e");
-end
-
+actions.e_key = function() kb.stroke("e"); end
 --@help Garage T
-actions.t_key = function()
-    kb.stroke("t");
+actions.t_key = function() kb.stroke("t"); end
+--@help Group Message M
+actions.m_key = function() kb.stroke("m"); end
+
+--@help Power Up - USE BOOST
+actions.powerup = function()
+    kb.stroke("space");
 end
 
---@help Group Message M
-actions.m_key = function()
-    kb.stroke("m");
+--@help Ride On - Give 1 Ride On (F3)
+actions.rideon = function()
+    kb.stroke("F3");
+end
+
+--@help Ride On Bomb - HOLD
+actions.rideon_bomb = function()
+    kb.stroke("F3");
+    libs.timer.timeout(function() kb.stroke("F3"); end, 300);
+    libs.timer.timeout(function() kb.stroke("F3"); end, 600);
+    libs.timer.timeout(function() kb.stroke("F3"); end, 900);
+    libs.timer.timeout(function() kb.stroke("F3"); end, 1200);
 end
